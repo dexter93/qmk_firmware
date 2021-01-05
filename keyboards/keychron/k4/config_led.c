0a8875d4d560d7094aa01a7223dd5354d77bdb99
Dimitris Mantzouranis <d3xter93@gmail.com>
2021-01-05 22:39:04 +0200

    Keychron K4 OpenRGB support

--- keyboards/keychron/k4/config_led.c
+++ keyboards/keychron/k4/config_led.c
@@ -26,4 +26,44 @@ led_config_t g_led_config = { {
    4, 4, 4, 0, 0, 0, 4, 0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 0
 } };
 
+#ifdef OPENRGB_ENABLE
+const openrgb_config_t g_openrgb_config =
+{
+    // Zones names
+    {"Keyboard"},
+
+    // Zones types
+    {OPENRGB_ZONE_TYPE_MATRIX},
+
+    // Zones sizes
+    {67},
+
+/* This servers as an example - the alt doesn't actually need this, only keyboards where their  
+//  g_led_config.matrix_co and layout doesn't actually represnet the true physical form of the keyboard 
+//  to get the correct values for the physical_to_hardware_location array
+//  I used the second array on the layout macro at alt.h
+*/
+#ifdef OPENRGB_USE_CUSTOM_MATRIX_MAP
+    // LED matrix map
+    {
+        {  0,      1,  2,      3,      4,      5,   6,      7,      8,      9,  10,  11,     12,  13,  14,  15,  16,  17,     18 },
+        { 19,     20, 21,     22,     23,     24,  25,     26,     27,     28,  29,  30,     31,  32,  33,  34,  35,  36,     37 },
+        { 38,     39, 40,     41,     42,     43,  44,     45,     46,     47,  48,  49,     50,  51,  52,  53,  54,  55,     56 },
+        { 57,     58, 59,     60,     61,     62,  63,     64,     65,     66,  67,  68, NO_LED,  69,  70,  71,  72,  73, NO_LED },
+        { 74, NO_LED, 75,     76,     77,     78,  79,     80,     81,     82,  83,  84,     85,  86,  87,  88,  89,  90,     91 },
+        { 92,     93, 94, NO_LED, NO_LED, NO_LED,  95, NO_LED, NO_LED, NO_LED,  96,  97,     98,  99, 100, 101, 102, 103, NO_LED },
+    },
+    // Key index to physical position map
+    {
+        {  0,      1,  2,      3,      4,      5,   6,      7,      8,      9,  10,  11,     12,  13,  14,  15,  16,  17,     18 },
+        { 19,     20, 21,     22,     23,     24,  25,     26,     27,     28,  29,  30,     31,  32,  33,  34,  35,  36,     37 },
+        { 38,     39, 40,     41,     42,     43,  44,     45,     46,     47,  48,  49,     50,  51,  52,  53,  54,  55,     56 },
+        { 57,     58, 59,     60,     61,     62,  63,     64,     65,     66,  67,  68, NO_LED,  70,  71,  72,  73,  74, NO_LED },
+        { 76, NO_LED, 78,     79,     80,     81,  82,     83,     84,     85,  86,  87,     88,  89,  90,  91,  92,  93,     94 },
+        { 95,     96, 97, NO_LED, NO_LED, NO_LED, 101, NO_LED, NO_LED, NO_LED, 105, 106,    107, 108, 109, 110, 111, 112, NO_LED },
+    },
+#endif
+};
+#endif
+
 #endif
--- keyboards/keychron/k4/config_led.h
+++ keyboards/keychron/k4/config_led.h
@@ -30,3 +30,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define LED_MATRIX_COL_PINS { A8, A9, A10, A11, A12, A13, A14, A15, B0, B1, B2, B3, B4, B5, B6, B7, B8, B9, B10 }
 
 #define DRIVER_LED_TOTAL (LED_MATRIX_ROWS * LED_MATRIX_COLS)
+
+#ifdef OPENRGB_ENABLE
+#define OPENRGB_ZONES_COUNT 1
+#define OPENRGB_USE_CUSTOM_MATRIX_MAP
+#ifdef OPENRGB_USE_CUSTOM_MATRIX_MAP
+    #define OPENRGB_MATRIX_COLUMNS 19
+    #define OPENRGB_MATRIX_ROWS 6
+#endif
+#endif
