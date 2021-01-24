b9c61fa16c73fd9056f7aa96d0ba3a13121112f8
Alexander <48211557+jedifindtrick@users.noreply.github.com>
2021-01-24 14:31:53 +0100

    Fixes
    Managed to successfully compile.
    Going to test flashing it and start working on the other layers.

--- keyboards/keychron/k6/keymaps/default/keymap.c
+++ keyboards/keychron/k6/keymaps/default/keymap.c
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include QMK_KEYBOARD_H
-#include keymap_swedish.h
+#include "keymap_swedish.h"
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
