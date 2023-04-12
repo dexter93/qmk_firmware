65789f5b56cf652bc6d2809952f937f5124803cd
Dimitris Mantzouranis <d3xter93@gmail.com>
2023-04-12 13:08:51 +0300

    serial: use approximate polled delay if no RTC available

--- platforms/chibios/drivers/serial.c
+++ platforms/chibios/drivers/serial.c
@@ -17,7 +17,16 @@
 // default wait implementation cannot be called within interrupt
 //   this method seems to be more accurate than GPT timers
 #if PORT_SUPPORTS_RT == FALSE
-#    error "chSysPolledDelayX method not supported on this platform"
+// chSysPolledDelayX method not supported on this platform, using inaccurate delay method.
+#    undef wait_us
+#    define wait_us(us)                                  \
+        do {                                             \
+            if (us != 0) {                               \
+                chThdSleepMicroseconds((us + 999)/1000); \
+            } else {                                     \
+                chThdSleepMicroseconds(1);               \
+            }                                            \
+        } while (0)
 #else
 #    undef wait_us
 // Force usage of polled waiting - in case WAIT_US_TIMER is activated
