409f8655724312cd01b677e8941ab02cd9bb27ab
Dimitris Mantzouranis <d3xter93@gmail.com>
2023-05-09 17:22:42 +0300

    c959: disable all matrix slave hooks

--- keyboards/cloudnine/c959/c959.c
+++ keyboards/cloudnine/c959/c959.c
@@ -5,12 +5,12 @@ void sw_i2c_delay(void) {
     for(uint8_t i=0; i<13; i++) __NOP();
 }
 /* Dummy slave mode */
-#define I2C_SLAVE_REG_COUNT 30
+//#define I2C_SLAVE_REG_COUNT 30
 
-uint8_t i2c_slave_reg[I2C_SLAVE_REG_COUNT];
+//uint8_t i2c_slave_reg[I2C_SLAVE_REG_COUNT];
 
-void i2c_slave_init(uint8_t address){}
-void i2c_slave_stop(void){}
+//void i2c_slave_init(uint8_t address){}
+//void i2c_slave_stop(void){}
 /* Debug */
 void keyboard_post_init_user(void) {
     debug_enable = true;
