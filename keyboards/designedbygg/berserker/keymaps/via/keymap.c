2f98d73f1327f4a05e02fca2398e6db8d5a65d4e
Dimitris Mantzouranis <d3xter93@gmail.com>
2023-05-19 12:26:35 +0300

    berserker: move encoder handling in the keymap

--- keyboards/designedbygg/berserker/keymaps/via/keymap.c
+++ keyboards/designedbygg/berserker/keymaps/via/keymap.c
@@ -53,11 +53,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (clockwise) {
-        tap_code_delay(KC_VOLU, 10);
-    } else {
-        tap_code_delay(KC_VOLD, 10);
-    }
-    return false;
-}
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][1][2] = {
+    [0] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [1]   = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI) },
+};
+#endif
