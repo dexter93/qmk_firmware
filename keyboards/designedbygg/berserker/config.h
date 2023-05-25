231ca05662bcf88e1d713f8f7fc10da2ae0629ee
Dimitris Mantzouranis <d3xter93@gmail.com>
2023-05-25 12:59:35 +0300

    meth. pure meth

--- keyboards/designedbygg/berserker/config.h
+++ keyboards/designedbygg/berserker/config.h
@@ -17,10 +17,13 @@
 #pragma once
 
 #include "config_common.h"
-
+#define DEBUG_MATRIX_SCAN_RATE
 /* Win Lock LED Indicator */
 #define LED_WIN_LOCK_PIN B12
-
+/* Board and GPIO setup */
+#define WAIT_US_TIMER GPTD2
+#define MATRIX_UNSELECT_DRIVE_HIGH
+#define GPIO_INPUT_PIN_DELAY 0
 /* RGB LED Config */
 #define LED_MATRIX_ROWS MATRIX_ROWS
 #define LED_MATRIX_ROW_CHANNELS 3
