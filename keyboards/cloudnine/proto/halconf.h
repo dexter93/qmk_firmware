f157ee06e5079c5bc9a46ed5c3c613a444f93f0d
Dimitris Mantzouranis <d3xter93@gmail.com>
2025-05-13 13:05:02 +0300

    cl9 proto: more testing

--- keyboards/cloudnine/proto/halconf.h
+++ keyboards/cloudnine/proto/halconf.h
@@ -1,10 +1,11 @@
 #pragma once
 #define HAL_USE_PAL TRUE
-//#define HAL_USE_GPT TRUE
+#define HAL_USE_GPT TRUE
 //#define PAL_USE_CALLBACKS TRUE
 #define HAL_USE_SERIAL TRUE
-//#define SN32_ST_USE_TIMER CT16B1
-#define SN32_UART_UART0_IRQ_PRIORITY          2
-#define SN32_I2C_I2C0_IRQ_PRIORITY         3
+#define SN32_ST_USE_TIMER SN32_TIM_CT16B1
+//#define SN32_UART_UART0_IRQ_PRIORITY          2
+//#define SN32_I2C_I2C0_IRQ_PRIORITY         3
+//#define SN32_GPT_CT16B1_IRQ_PRIORITY         1
 
 #include_next <halconf.h>
--- keyboards/cloudnine/proto/mcuconf.h
+++ keyboards/cloudnine/proto/mcuconf.h
@@ -1,5 +1,5 @@
 #pragma once
 #define SN32_SERIAL_USE_UART0 TRUE
 #define SN32_I2C_USE_I2C0 TRUE
-//#define SN32_GPT_USE_CT16B1 TRUE
+#define SN32_GPT_USE_CT16B0 TRUE
 #include_next <mcuconf.h>
