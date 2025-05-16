060dd6796e9b65e0a7da89e1cb702a22781b2094
Dimitris Mantzouranis <d3xter93@gmail.com>
2025-05-16 23:03:39 +0300

    Revert "Reapply "TEST: rgb matrix: guard led indexing""
    This reverts commit e44111aee8f9be6fd71c65634724d1b97896e611.

--- quantum/rgb_matrix/rgb_matrix.c
+++ quantum/rgb_matrix/rgb_matrix.c
@@ -142,22 +142,9 @@ uint8_t rgb_matrix_map_row_column_to_led(uint8_t row, uint8_t column, uint8_t *l
 void rgb_matrix_update_pwm_buffers(void) {
     rgb_matrix_driver.flush();
 }
-__attribute__((weak)) int rgb_matrix_led_index(int index) {
-#if defined(RGB_MATRIX_SPLIT)
-    if (is_keyboard_left() && index >= k_rgb_matrix_split[0]) {
-        return -1;
-    }
-    if (!is_keyboard_left() && index < k_rgb_matrix_split[0]) {
-        return -1;
-    }
-
-#endif
-    return index;
-}
 
 void rgb_matrix_set_color(int index, uint8_t red, uint8_t green, uint8_t blue) {
-    int mapped_index = rgb_matrix_led_index(index);
-    if (mapped_index >= 0) rgb_matrix_driver.set_color(mapped_index, red, green, blue);
+    rgb_matrix_driver.set_color(index, red, green, blue);
 }
 
 void rgb_matrix_set_color_all(uint8_t red, uint8_t green, uint8_t blue) {
