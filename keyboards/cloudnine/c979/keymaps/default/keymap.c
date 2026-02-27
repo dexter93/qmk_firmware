fc664ade1aab1537a639bdf7c87942cbed9d4976
Dimitris Mantzouranis <d3xter93@gmail.com>
2026-02-27 20:23:25 +0200

    c979: more keymap

--- keyboards/cloudnine/c979/keymaps/default/keymap.c
+++ keyboards/cloudnine/c979/keymaps/default/keymap.c
@@ -41,11 +41,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [WIN_BASE] = LAYOUT_fullsize_ansi_split_spc(
         PC_LOCK,
         RM_NEXT,     KC_ESC,    KC_F1,   KC_F2,   KC_F3,   KC_F4,            KC_F5,            KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,     KC_F12,     KC_PSCR, KC_SCRL, KC_PAUS,                               KC_MUTE,
-        _______,     KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,             KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,     KC_BSPC,    KC_INS,  KC_HOME, KC_PGUP,    KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
-        _______,     KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,    KC_BSLS,    KC_DEL,  KC_END,  KC_PGDN,    KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
-        _______,     KC_CAPS,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,             KC_ENT,                                   KC_P4,   KC_P5,   KC_P6,
-        _______,     KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,             KC_RSFT,             KC_UP,               KC_P1,   KC_P2,   KC_P3,   KC_PENT,
-        RGB_TEST,    KC_LCTL,   KC_LWIN, KC_LALT,          KC_SPC,                             KC_SPC,                    KC_RALT, KC_RWIN, MO(WIN_FN), KC_RCTL,    KC_LEFT, KC_DOWN, KC_RGHT,    KC_P0,            KC_PDOT
+        KC_HOME,     KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,             KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,     KC_BSPC,    KC_INS,  KC_HOME, KC_PGUP,    KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
+        KC_END,      KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,    KC_BSLS,    KC_DEL,  KC_END,  KC_PGDN,    KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
+        KC_COPY,     KC_CAPS,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,             KC_ENT,                                   KC_P4,   KC_P5,   KC_P6,
+        KC_PSTE,     KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,             KC_RSFT,             KC_UP,               KC_P1,   KC_P2,   KC_P3,   KC_PENT,
+        MO(WIN_FN),  KC_LCTL,   KC_LWIN, KC_LALT,          KC_SPC,                             KC_SPC,                    KC_RALT, KC_RWIN, MO(WIN_FN), KC_RCTL,    KC_LEFT, KC_DOWN, KC_RGHT,    KC_P0,            KC_PDOT
     ),
     [WIN_FN] = LAYOUT_fullsize_ansi_split_spc(
         _______,
@@ -54,16 +54,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,     _______, _______, _______, _______, _______, _______,             _______, _______, _______, _______, _______, _______, _______, _______,    RM_HUED, RM_SATD, RM_VALD,    _______, _______, _______, _______,
         _______,     _______, _______, _______, _______, _______, _______,             _______, _______, _______, _______, _______, _______,          QK_BOOT,                                  _______, _______, _______,
         _______,     _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______,          _______,             RM_SPDU,             _______, _______, _______, _______,
-        _______,     _______, _______, _______,          _______,                               _______,                   _______, _______, _______, RM_TOGG,    RM_PREV, RM_SPDD, RM_NEXT,    _______,          _______
+        RGB_TEST,    _______, _______, _______,          _______,                               _______,                   _______, _______, _______, RM_TOGG,    RM_PREV, RM_SPDD, RM_NEXT,    _______,          _______
     ),
     [MAC_BASE] = LAYOUT_fullsize_ansi_split_spc(
         MC_LOCK,
         RM_NEXT,     KC_ESC,    KC_BRID, KC_BRIU, KC_MCTL, KC_LPAD,          KC_F5,            KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,     KC_F12,     KC_PSCR, KC_SCRL, KC_PAUS,                               KC_MUTE,
-        _______,     KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,             KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,     KC_BSPC,    KC_INS,  KC_HOME, KC_PGUP,    KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
-        _______,     KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,    KC_BSLS,    KC_DEL,  KC_END,  KC_PGDN,    KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
-        _______,     KC_CAPS,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,             KC_ENT,                                   KC_P4,   KC_P5,   KC_P6,
-        _______,     KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,             KC_RSFT,             KC_UP,               KC_P1,   KC_P2,   KC_P3,   KC_PENT,
-        RGB_TEST,    KC_LCTL,   KC_LOPT, KC_LCMD,          KC_SPC,                             KC_SPC,                    KC_RCMD, KC_ROPT, MO(MAC_FN), KC_RCTL,    KC_LEFT, KC_DOWN, KC_RGHT,    KC_P0,            KC_PDOT
+        KC_HOME,     KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,             KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,     KC_BSPC,    KC_INS,  KC_HOME, KC_PGUP,    KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
+        KC_END,      KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,    KC_BSLS,    KC_DEL,  KC_END,  KC_PGDN,    KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
+        KC_COPY,     KC_CAPS,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,             KC_ENT,                                   KC_P4,   KC_P5,   KC_P6,
+        KC_PSTE,     KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,             KC_RSFT,             KC_UP,               KC_P1,   KC_P2,   KC_P3,   KC_PENT,
+        MO(MAC_FN),  KC_LCTL,   KC_LOPT, KC_LCMD,          KC_SPC,                             KC_SPC,                    KC_RCMD, KC_ROPT, MO(MAC_FN), KC_RCTL,    KC_LEFT, KC_DOWN, KC_RGHT,    KC_P0,            KC_PDOT
     ),
     [MAC_FN] = LAYOUT_fullsize_ansi_split_spc(
         _______,
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,     _______, _______, _______, _______, _______, _______,             _______, _______, _______, _______, _______, _______, _______, _______,    RM_HUED, RM_SATD, RM_VALD,    _______, _______, _______, _______,
         _______,     _______, _______, _______, _______, _______, _______,             _______, _______, _______, _______, _______, _______,          QK_BOOT,                                  _______, _______, _______,
         _______,     _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______,          _______,             RM_SPDU,             _______, _______, _______, _______,
-        _______,     _______, _______, _______,          _______,                               _______,                   _______, _______, _______, RM_TOGG,    RM_PREV, RM_SPDD, RM_NEXT,    _______,          _______
+        RGB_TEST,    _______, _______, _______,          _______,                               _______,                   _______, _______, _______, RM_TOGG,    RM_PREV, RM_SPDD, RM_NEXT,    _______,          _______
     )
 };
 
